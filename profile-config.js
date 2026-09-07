838e250741e677e9ce54b5b966e0b5b3c2bd0c5b
muangtuet <pimolpan150838@gmail.com>
2026-09-07 11:36:20 +0700

    Update profile-config.js

--- profile-config.js
+++ profile-config.js
@@ -1,2 +1,2 @@
 window.STUDENT_PROFILE_WEB_APP_URL =
-  'https://script.google.com/macros/s/AKfycbzZPKCjqrvptWM7nVyquVLeU2GlKrS2UtljX1vQCqDHR2UVsL_-Tyh5BqVctKUwsv1C/exec';
+  'https://script.google.com/macros/s/AKfycbx5RKBBfaE0jipxP8W-pk2Ey31OgGZfvy9OrKRyeNnAGuRRPMkBltoAD4BZRg_4X3CV6g/exec';
