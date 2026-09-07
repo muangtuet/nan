45bda93d487849cb416bdebd62c3c6dd0ea7ef67
muangtuet <pimolpan150838@gmail.com>
2026-09-07 11:34:51 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.APP_CONFIG = Object.freeze({
-    API_URL: 'https://script.google.com/macros/s/AKfycbw7gUWih1UGW37lxpi1jj98G0k35iVN7FVRMP-E4HeMs0ea54oTtAI9f2uj4OYHPx4w/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbwcf23uFYxO__QmROoii6xfrV2om6gEIBnGgtzxtWaD5XygGCJJISRbZ3n_DDFjkz1Q/exec'
   });
 })();
